7be6ca0f8c8b606362c3379c488c058600e311b0
Ankur Roy <94541480+ankur0360@users.noreply.github.com>
2022-05-21 22:25:13 +0530

    Create check_student_absent_or_not.py

---
+++ User_defined_func/check_student_absent_or_not.py
@@ -0,0 +1,8 @@
+def ank(a):
+	a = [1,4,5,2]   # including roll numbber list 
+	if b in a:      # checking if b is present in a's any index
+		print('Present')
+	else:
+		print('Absent')
+b = int(input('Enter roll : '))
+ank(b)            # function calling 
